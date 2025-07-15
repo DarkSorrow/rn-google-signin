6b009c61dd6d2f8be29ae5e54a1dd9098b31f52f
DarkSorrow <darksorrow.slade@gmail.com>
2025-07-15 14:59:46 +0200

    fix: error name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import GoogleSignIn from './GoogleSignin';
+import GoogleSignIn from './GoogleSignIn';
 
-export { default as GoogleSignIn } from './GoogleSignin';
+export { default as GoogleSignIn } from './GoogleSignIn';
 export * from './types';
 
 // Export the singleton instance as the default export
-export default GoogleSignIn; 
+export default GoogleSignIn;
