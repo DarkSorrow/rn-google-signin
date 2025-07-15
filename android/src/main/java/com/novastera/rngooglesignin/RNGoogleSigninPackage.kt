62c5b9cabce62f5ac641398b2b7e50738fd277f9
DarkSorrow <darksorrow.slade@gmail.com>
2025-07-16 00:01:11 +0200

    feat: more changes

--- android/src/main/java/com/novastera/rngooglesignin/RNGoogleSigninPackage.kt
+++ android/src/main/java/com/novastera/rngooglesignin/RNGoogleSigninPackage.kt
@@ -1,12 +1,12 @@
 package com.novastera.rngooglesignin
 
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class RNGoogleSigninPackage : BaseReactPackage() {
+class RNGoogleSigninPackage : TurboReactPackage() {
 
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? =
         if (name == RNGoogleSigninModule.NAME) {
